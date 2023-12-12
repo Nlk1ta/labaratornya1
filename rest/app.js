9624e12feb161ebb0d81a9381d361879d9f1e558
Nlk1ta <shmidt.n1kita@yandex.ru>
2023-12-12 22:59:19 +0700

    laba1
    rest

---
+++ rest/app.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+const {v4} = require('uuid')
+
+const CONTACTS = [
+    { id: v4(), name: 'Nikita', value: '432432', marked: false}
+]
+
+app.use(express.json())
+
+//GET
+app.get('/api/contacts', (req, res) => {
+    setTimeout(() => {
+        res.status(200).json(CONTACTS)
+    })
+})
+
+//POST
+app.post('/api/contacts', (req, res) => {
+    const contact = { ...req.body, id: v4(), marked: false }
+    CONTACTS.push(contact)
+    res.status(201).json(contact)
+})
+
+//DELETE
+app.delete('/api/contacts/:id', (req, res) => {
+    CONTACTS = CONTACTS.filter(c => c.id !== req.params.id)
+    res.status(200).json({message: 'Контакт был удалён'})
+})
+
+//PUT
+app.put('/api/contacts/:id', (req, res) => {
+    const idx = CONTACTS.findIndex(c => c.id === req.params.id)
+    CONTACTS[idx] = req.body
+    res.json(CONTACTS[idx])
+})
+    
+
+app.use(express.static(path.resolve(__dirname, 'idk'))) // делаем статическую папку, чтобы fronted.js не потерялась
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'idk', 'index.html'))
+})
+
+app.listen(3000, () => console.log('Server has been started on port 3000...'))
